ce2c760b9fc922de7e5c059b4dfe1a97dcb6159b
cezarytworzewski92 <cezary.tworzewski@gmail.com>
2018-01-07 01:04:40 +0100

    Button value show on the display

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-var form = document.querySelectorAll("form");
 
-function clickNumber() {
+
+var button = document.querySelectorAll('.button');
+var btnDisplay = document.getElementById("buttonDisplay");
+
+for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', numberOutputDisplay);
+}
+
+function numberOutputDisplay() {
+    btnDisplay.value = btnDisplay.value + this.value;
+    
+    if (this.value === "C") {
+        btnDisplay.value = "0";
+    }
+   
+   
     
 }
